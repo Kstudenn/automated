d628235efa01585eec00e34bc6062dab35ce3f5b
Kacper Studenny <117851213+Kstudenn@users.noreply.github.com>
2023-06-04 18:03:44 +0200

    change

--- example.py
+++ example.py
@@ -65,7 +65,7 @@ def check_reactor_temperature(temperature_celsius):
     Checks whether temperature is above max_temperature
     and returns a status.
     """
-    #from reactor import max_temperature
+    max_temperature=100
     if temperature_celsius > max_temperature:
         status = 1
     else:
@@ -73,7 +73,6 @@ def check_reactor_temperature(temperature_celsius):
     return status
 
 def test_set_temp(monkeypatch):
-    monkeypatch.setattr(reactor, "max_temperature", 100)
     assert check_reactor_temperature(99)  == 0
     assert check_reactor_temperature(100) == 0   # boundary cases easily go wrong
     assert check_reactor_temperature(101) == 1
